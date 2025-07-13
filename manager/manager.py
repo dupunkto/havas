6d076cd0fe6dcd89c0c8c25dfc61ff0fa6e252b6
Gijs <gijstenberg@gmail.com>
2025-07-13 15:27:15 +0200

    Contiune refactoring manager

--- manager/manager.py
+++ manager/manager.py
@@ -15,13 +15,22 @@ manager_bp = Blueprint(
 @manager_bp.route("/")
 def index():
     articles = Article.query.all()
-    
+
     return render_template("manager_index.html", articles=articles)
+
 
 @manager_bp.route("/add_article")
 def add_article():
     return "", 200
 
+
 @manager_bp.route("/editor/<string:id>")
 def editor(id):
+    article = Article.query.get(id)
+
+    return render_template("manager_editor.html", article=article)
+
+
+@manager_bp.route("/save_api", methods=["POST"])
+def save_api():
     return "", 200
